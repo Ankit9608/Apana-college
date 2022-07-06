3cbb63e3689ab4339e9d2da6cccf3a7f7ba5fa1c
Ankit9608 <ankitpatilavp@gmail.com>
2022-07-06 22:28:33 +0530

    comit

---
+++ reverse_stack.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stack>
+using namespace std;
+void InsetAtBottom(stack<int> &st, int ele)
+{
+    if (st.empty())
+    {
+        st.push(ele);
+        return;
+    }
+    int topele = st.top();
+    st.pop();
+    InsetAtBottom(st, ele);
+    st.push(topele);
+}
+
+void reverse(stack<int> &st)
+{
+    if (st.empty())
+    {
+        return;
+    }
+
+    int ele = st.top();
+    st.pop();
+    reverse(st);
+    InsetAtBottom(st, ele);
+}
+int main()
+{
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    reverse(st);
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+}
