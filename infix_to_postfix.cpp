1bdd47039853294adff34e9cdf2055ab9cdb5e23
Ankit9608 <ankitpatilavp@gmail.com>
2022-07-09 23:55:15 +0530

    c

--- infix_to_postfix.cpp
+++ infix_to_postfix.cpp
@@ -1,6 +1,80 @@
+// #include <iostream>
+// #include <math.h>
+// #include <stack>
+// using namespace std;
+// int prec(char a)
+// {
+//     if (a == '^')
+//     {
+//         return 3;
+//     }
+//     else if (a == '*' || a == '/')
+//     {
+//         return 2;
+//     }
+//     else if (a == '+' || a == '-')
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return -1;
+//     }
+// }
+// string infixtopostfix(string s)
+// {
+//     stack<char> st;
+//     string res = "";
+//     for (int i = 0; i < s.length(); i++)
+//     {
+//         if (s[i] == '(')
+//         {
+//             st.push(s[i]);
+//         }
+//         else if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+//         {
+//             res += s[i];
+//         }
+//         else if (s[i] == ')')
+//         {
+//             while (!st.empty() && st.top() != '(')
+//             {
+//                 res += st.top();
+//                 st.pop();
+//             }
+//             if (!st.empty())
+//             {
+//                 st.pop();
+//             }
+//         }
+//         else
+//         {
+//             while (!st.empty() && prec(st.top()) > prec(s[i]))
+//             {
+//                 res += st.top();
+//                 st.pop();
+//             }
+//             st.push(s[i]);
+//         }
+//     }
+//     while (!st.empty())
+//     {
+//         res += st.top();
+//         st.pop();
+//     }
+//     return res;
+// }
+// int main()
+// {
+//     cout << infixtopostfix("(a-b/c)*(a/k-l)");
+
+//     return 0;
+// }
 #include <iostream>
-#include <math.h>
+#include <bits/stdc++.h>
 #include <stack>
+#include <math.h>
+
 using namespace std;
 int prec(char a)
 {
@@ -23,8 +97,8 @@ int prec(char a)
 }
 string infixtopostfix(string s)
 {
-    stack<char> st;
     string res = "";
+    stack<char> st;
     for (int i = 0; i < s.length(); i++)
     {
         if (s[i] == '(')
@@ -62,11 +136,11 @@ string infixtopostfix(string s)
         res += st.top();
         st.pop();
     }
+
     return res;
 }
 int main()
 {
     cout << infixtopostfix("(a-b/c)*(a/k-l)");
-
     return 0;
 }
